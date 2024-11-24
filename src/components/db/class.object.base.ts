c1bf6438f7b15131e75d68714c4c8beb28c7519e
ClanersKevin <itzayhr@gmail.com>
2024-11-24 00:49:17 -0800

    Update class.object.base.ts

--- src/components/db/class.object.base.ts
+++ src/components/db/class.object.base.ts
@@ -10,23 +10,27 @@ export default abstract class BaseObject {
 	#idIdentity?: number;
 	[key: string]: any;
 
-	protected constructor(data?: Partial<BaseObject>) {
-		if (data) {
-			const idField = (this.constructor as typeof BaseObject).idIdentityName;
-			if (idField && data[idField]) {
-				this.#idIdentity = data[idField] as number;
+	protected constructor(_data?: Partial<BaseObject>) {
+		if (_data) {
+			const _idField = (this.constructor as typeof BaseObject).idIdentityName;
+			if (_idField && _data[_idField]) {
+				this.#idIdentity = _data[_idField] as number;
 				this.storeInstance();
 			}
-			Object.assign(this, data);
+			Object.assign(this, _data);
 		}
 	}
 
-	// Método para obtener el ID de la instancia actual
+	/**
+	 * Método para obtener el ID de la instancia actual
+	 */
 	public getId(): number | undefined {
 		return this.#idIdentity;
 	}
 
-	// Método estático para obtener propiedades permitidas de la clase hija
+	/**
+	 * Método estático para obtener propiedades permitidas de la clase hija
+	 */
 	protected static getProperties(): string[] {
 		return this.columns;
 	}
@@ -37,7 +41,7 @@ export default abstract class BaseObject {
 		// Comprobamos que todas las propiedades del objeto sean válidas
 		for (const prop in this) {
 			if (!validColumns.includes(prop)) {
-				throw new Error(`La propiedad '${prop}' no es válida para la tabla ${this.constructor.name}.`);
+				throw new Error(`La propiedad '${prop}' no es válida para la clase ${this.constructor.name}.`);
 			}
 		}
 	}
@@ -46,73 +50,76 @@ export default abstract class BaseObject {
 	 * Método genérico para obtener una instancia por ID.
 	 * Usa un tipo genérico para inferir la clase hija.
 	 */
-	public static async getInstance<T extends BaseObject>(this: new (data: Partial<T>) => T, _id?: number): Promise<T> {
-		const classConstructor = this.constructor as typeof BaseObject;
-		const className = classConstructor.name;
-
-		if (_id) {
-			// Verificar si la instancia ya está en caché
-			if (classConstructor.instances && classConstructor.instances[className] && classConstructor.instances[className]?.[_id]) {
-				return classConstructor.instances[className][_id] as T;
-			}
-
-			// Si no está en la caché, obtenerla de la base de datos
-			return BaseObject.getInstanceFromDB(_id, this);
+	public static async getInstance<T extends BaseObject>(this: new (_data: Partial<T>) => T, _id?: number): Promise<T> {
+		if (!_id) {
+			return new this({});
 		}
 
-		return new this({});
+		const _className = this.name;
+
+		// Verificar si la instancia ya está en caché
+		if (BaseObject.instances && BaseObject.instances[_className] && BaseObject.instances[_className]?.[_id]) {
+			return BaseObject.instances[_className][_id] as T;
+		}
+
+		// Si no está en la caché, obtenerla de la base de datos
+		return BaseObject.getInstanceFromDB(_id, this);
 	}
 
-	private static async getInstanceFromDB<T extends BaseObject>(_id: number, classConstructor: new (data: Partial<T>) => T): Promise<T> {
-		const db: any = DB.getInstance(); // Usamos cualquier tipo, ya que DB es un singleton
+	private static async getInstanceFromDB<T extends BaseObject>(_id: number, _classConstructor: new (_data: Partial<T>) => T): Promise<T> {
+		const _db: any = DB.getInstance(); // Usamos cualquier tipo, ya que DB es un singleton
 
-		const idIdentityName = (classConstructor as any).idIdentityName;
-		if (!idIdentityName) {
-			throw new Error(`Llave primaria de la tabla no definida por el objeto ${classConstructor.name}`);
+		const _idIdentityName = (_classConstructor as any).idIdentityName; // Accedemos al nombre de la llave primaria a través del constructor de la clase
+		if (!_idIdentityName) {
+			throw new Error(`Llave primaria de la tabla no definida por el objeto ${_classConstructor.name}`);
 		}
 
-		const tableName = (classConstructor as any).tableName; // Accedemos al nombre de la tabla a través del constructor de la clase
-		if (!tableName) {
-			throw new Error(`Nombre de tabla no definido por el objeto ${classConstructor.name}`);
+		const _tableName = (_classConstructor as any).tableName; // Accedemos al nombre de la tabla a través del constructor de la clase
+		if (!_tableName) {
+			throw new Error(`Nombre de tabla no definido por el objeto ${_classConstructor.name}`);
 		}
 
-		const result = await db.query(`SELECT * FROM ${tableName} WHERE ${idIdentityName} = ?`, [_id]);
-		if (result.length === 0) {
-			throw new Error(`No se encontró el registro con ${idIdentityName} = ${_id} en la tabla ${tableName}`);
+		const _result = await _db.query(`SELECT * FROM ${_tableName} WHERE ${_idIdentityName} = ?`, [_id]);
+		if (_result.length === 0) {
+			throw new Error(`No se encontró el registro con ${_idIdentityName} = ${_id} en la tabla ${_tableName}`);
 		}
 
-		return new classConstructor(result[0]); // Usamos el constructor de la clase hija para crear la instancia
+		return new _classConstructor(_result[0]); // Usamos el constructor de la clase hija para crear la instancia
 	}
 
-	// Método para guardar la instancia en el registro estático
+	/**
+	 * Método para guardar la instancia en el registro estático
+	 */
 	private storeInstance(): void {
-		const classConstructor = this.constructor as typeof BaseObject;
-		const className = classConstructor.name;
+		const _classConstructor = this.constructor as typeof BaseObject;
+		const _className = _classConstructor.name;
 		if (!this.#idIdentity) return;
 
 		// Accedemos a la propiedad estática 'instances' desde el constructor
-		if (!classConstructor.instances[className]) {
-			classConstructor.instances[className] = {};
+		if (!_classConstructor.instances[_className]) {
+			_classConstructor.instances[_className] = {};
 		}
 
-		classConstructor.instances[className][this.#idIdentity] = this;
+		_classConstructor.instances[_className][this.#idIdentity] = this;
 	}
 
-	// Método estático para eliminar una instancia del registro
+	/**
+	 * Método estático para eliminar una instancia del registro
+	 */
 	public async removeInstance(id?: number) {
-		const classConstructor = this.constructor as typeof BaseObject;
-		const className = this.constructor.name;
+		const _classConstructor = this.constructor as typeof BaseObject;
+		const _className = this.constructor.name;
 
-		console.log("classConstructor.instances", classConstructor.instances);
+		if (!_classConstructor.instances[_className]) return;
 
-		if (!classConstructor.instances[className]) return;
-
-		if (id && classConstructor.instances[className][id]) {
-			delete classConstructor.instances[className][id];
+		if (id && _classConstructor.instances[_className][id]) {
+			delete _classConstructor.instances[_className][id];
 		}
 	}
 
-	// Método para guardar (insertar o actualizar) el objeto
+	/**
+	 * Método para guardar el objeto
+	 */
 	public async save(): Promise<boolean> {
 		this.validateProperties();
 
@@ -123,38 +130,48 @@ export default abstract class BaseObject {
 			throw new Error(`Nombre de tabla no definido por el objeto ${this.constructor.name}`);
 		}
 
-		if (this.idIdentity) {
-			return await this.update(db, tableName); // Si tiene id, actualizamos
+		if (this.#idIdentity) {
+			return await this._update(db, tableName);
 		} else {
-			return await this.insert(db, tableName); // Si no tiene id, insertamos
+			return await this._insert(db, tableName);
 		}
 	}
 
-	// Función para insertar un nuevo registro
-	private async insert(db: any, tableName: string): Promise<boolean> {
-		const columns = Object.keys(this).filter((key) => key !== this.idIdentityName);
-		const placeholders = columns.map(() => "?").join(", ");
-		const values = columns.map((key) => this[key]);
+	/**
+	 * Función para insertar un nuevo registro
+	 */
+	private async _insert(_db: any, _tableName: string): Promise<boolean> {
+		const _columns = Object.keys(this).filter((key) => key !== this.idIdentityName);
+		const _placeholders = _columns.map(() => "?").join(", ");
+		const _values = _columns.map((key) => this[key]);
 
-		const query = `INSERT INTO ${tableName} (${columns.join(", ")}) VALUES (${placeholders})`;
-		const result = await db.query(query, values);
-		if (result.affectedRows > 0) {
-			this.idIdentity = result.insertId;
+		const _query = `INSERT INTO ${_tableName} (${_columns.join(", ")}) VALUES (${_placeholders})`;
+		const _result = await _db.query(_query, _values);
+
+		if (_result.affectedRows > 0) {
+			this.#idIdentity = _result.insertId;
+			this.storeInstance();
 			return true;
 		}
+
 		return false;
 	}
 
-	// Función para actualizar un registro existente
-	private async update(db: any, tableName: string): Promise<boolean> {
-		const columns = Object.keys(this).filter((key) => key !== "id");
-		const setClause = columns.map((key) => `\`${key}\` = ?`).join(", ");
-		const values = columns.map((key) => this[key]);
+	/**
+	 * Función para actualizar un registro existente
+	 */
+	private async _update(_db: any, _tableName: string): Promise<boolean> {
+		const _classConstructor = this.constructor as typeof BaseObject;
+		const _columns = Object.keys(this).filter((key) => key !== _classConstructor.idIdentityName);
+		const _placeholders = _columns.map((key) => `\`${key}\` = ?`).join(", ");
+		const _values = _columns.map((key) => this[key]);
 
-		const query = `UPDATE ${tableName} SET ${setClause} WHERE ${this.idIdentityName} = ?`;
-		values.push(this.idIdentity);
+		const _query = `UPDATE ${_tableName} SET ${_placeholders} WHERE ${_classConstructor.idIdentityName} = ?`;
+		_values.push(this.#idIdentity);
+		console.log(_query);
+		console.log(_values);
 
-		const result = await db.query(query, values);
-		return result.affectedRows > 0;
+		const _result = await _db.query(_query, _values);
+		return _result.affectedRows > 0;
 	}
 }
